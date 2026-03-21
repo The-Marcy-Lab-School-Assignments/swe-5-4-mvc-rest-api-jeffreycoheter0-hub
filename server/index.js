fb0f93f2f0f8b3b8e3e8efb252f4a31c5cd58450
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2026-03-20 21:21:37 -0400

    Did not get far at all, but I do know what I need to do

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { takeCoverage } = require('v8');
 
 const app = express();
 const pathToFrontend = path.join(__dirname, '../frontend');
@@ -40,18 +41,45 @@ const todos = [
 
 // TODO: GET /api/todos
 // Response: 200, array of all todos
+const listTodos = (req, res) => {
+  res.send(todos);
+};
+app.get('/api/todos', listTodos);
 
 
 // TODO: GET /api/todos/:id
 // Response: 200, single todo object
 // Error: 404 if no todo with that id
+const singleTask = (req, res) => {
+  const { id } = req.params;
+  const task = todos.find(task => task.id === Number(id));
+
+  if (!id) {
+    res.status(404).send({
+      message: `No task with the id ${id}`
+    });
+  }
+  res.send(task);
+};
+app.get('/api/todos/:id', singleTask);
 
 
 // TODO: POST /api/todos
 // Request body: { task }
 // Response: 201, the newly created todo object
 // Error: 400 if task is missing from the request body
+const createTask = (req, res) => {
+  const { taskName } = req.body;
 
+  if (!taskName) {
+    return res.status(400).send({
+      message: 'Invalid Task Name'
+    });
+  }
+
+  const newTask = { taskName, id: getId() };
+  res.send(todos.push(newTask));
+};
 
 // TODO: PATCH /api/todos/:id
 // Request body: { isDone }
